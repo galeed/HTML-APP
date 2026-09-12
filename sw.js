a311615a3fa28efc788b57bf4d085074ff4d06b2
galeedx <113880595+galeed@users.noreply.github.com>
2026-09-12 16:41:56 -0600

    Crear sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+self.addEventListener('fetch', (event) => {
+ 
+  if (event.request.url.startsWith('blob:') || event.request.headers.get('range')) {
+    return; 
+  }
+
+});
+
+
+const CACHE_NAME = 'html-app';
+const ASSETS = [
+  './',
+  'index.html'
+];
+
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
